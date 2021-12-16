a8b7dc2fdd4067fcda4cb7ccfc1ba03aceb55664
numhebzaba <num.ptp@gmail.com>
2021-12-16 12:29:11 +0700

    no message

---
+++ final_8.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Final_8
+{
+    class Node
+    {
+        public string Value;
+        public Node Next;
+
+        public Node(string value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+    class Stack
+    {
+        private Node root;
+
+        public void Push(Node node)
+        {
+            if (IsRootNull())
+            {
+                root = node;
+            }
+            else
+            {
+                node.Next = root;
+                root = node;
+            }
+        }
+
+        public Node Pop()
+        {
+            Node node = root;
+            root = root.Next;
+            node.Next = null;
+            return node;
+        }
+
+        public bool IsRootNull()
+        {
+            return root is null;
+        }
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Stack productionLine = new Stack();
+            Stack assembleLine = new Stack();
+            Stack tempStock = new Stack();
+
+            int targetNumberOfProduct = int.Parse(Console.ReadLine());
+            int numberOfProduct = 0;
+
+            while (numberOfProduct<targetNumberOfProduct)
+            {
+                string part = Console.ReadLine();
+                Node node = new Node(part);
+
+                productionLine.Push(node);
+            }
+            
+
+        }
+    }
+}
---
+++ final_9.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace final_9
+{
+    class Node
+    {
+        public Node Left;
+        public Node Right;
+        public Node Next;
+        public string Name;
+
+        public Node(string nameValue)
+        {
+            Name = nameValue;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    class Queue
+    {
+        private Node root;
+
+        public void Push(Node node)
+        {
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node ptr = root;
+                while (ptr.Next != null)
+                {
+                    ptr = ptr.Next;
+                }
+                ptr.Next = node;
+            }
+        }
+
+        public Node Pop()
+        {
+            Node node = root;
+            if (root != null)
+            {
+                root = root.Next;
+                node.Next = null;
+            }
+            return node;
+        }
+
+        public int GetLength()
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                Node ptr = root;
+                int length = 1;
+                while (ptr.Next != null)
+                {
+                    ptr = ptr.Next;
+                    length++;
+                }
+                return length;
+            }
+        }
+
+        public override string ToString()
+        {
+            string message = "";
+            if (root != null)
+            {
+                Node ptr = root;
+                message = message + "," + root;
+                while (ptr.Next != null)
+                {
+                    ptr = ptr.Next;
+                    message = message + "," + ptr;
+                }
+            }
+            return message;
+        }
+
+        public int CheckskillPoint(Node skillName)
+        {
+            Node node = root;
+            int skillpointUse = 0;
+            while (true)
+            {
+                if (root != null)
+                {
+                    if(root == skillName)
+                    {
+                        skillpointUse++;
+                    }
+                    root = root.Next;
+                }
+                else // root = null
+                {
+                    return 0;
+                }
+            }
+            return skillpointUse;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue skillQueue = new Queue();
+            int skillPointUse = 0;
+
+            Console.WriteLine("Input skill name:");
+            string skillName = Console.ReadLine();
+            if(skillName == "?")
+            {
+                while (true)
+                {
+                    string skillUp = Console.ReadLine();
+                    if (skillUp == "?")
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Add dependency for {0} ?  (Y/N) :",skillName);
+                char ans = char.Parse(Console.ReadLine());
+                if(ans == 'Y')
+                {
+                    Node node = new Node(skillName);
+                    skillQueue.Push(node);
+
+                    Console.WriteLine("Input skill name:");
+                    string nextSkillName = Console.ReadLine();
+                    Console.WriteLine("Add dependency for {0} ?  (Y/N) :", nextSkillName);
+                    char nextAns = char.Parse(Console.ReadLine());
+                    if (nextAns == 'Y')
+                    {
+
+                    }
+                    else if(nextAns == 'N')
+                    {
+
+                    }
+
+
+                }
+                else if(ans == 'N')
+                {
+
+                }
+            }
+        }
+    }
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ                  
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ                               
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ            
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ            
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ             
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ              
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ                 
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ           
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ        
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ           
+    // ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ ยงธงขาวค้าบบบ
+    //
+    //
+    //
+    //
+    //
+    //
+}
